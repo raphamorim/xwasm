b969ae7e3de06031d39994b39e9731c84c0cf9db
Raphael Amorim <rapha850@gmail.com>
2019-06-06 22:53:47 -0300

    demo

--- use-wasm/useWasm.js
+++ use-wasm/useWasm.js
@@ -1,17 +1,27 @@
-// imports is an optional imports object
 function useWasm(fileName) {
-  console.log(1)
   if (!('WebAssembly' in window)) {
     console.warn('you need a browser with wasm support enabled');
   }
 
+  const memory = new WebAssembly.Memory({ initial: 256 });
+  const importObj = {
+      env: {
+          abortStackOverflow: () => { throw new Error('overflow'); },
+          table: new WebAssembly.Table({
+            initial: 0, element: 'anyfunc'
+          }),
+          __table_base: 0,
+          memory: memory,
+          __memory_base: 1024,
+          STACKTOP: 0,
+          STACK_MAX: memory.buffer.byteLength,
+      }
+  };
 
-  const response = await  WebAssembly.compileStreaming(fetch(fileName));
-
-
-  console.log(response);
-  // return fetch(fileName)
-  //   .then(response => response.arrayBuffer())
-  //   .then(buffer => WebAssembly.compile(buffer))
-  //   .then(module => {return new WebAssembly.Instance(module) });
+  WebAssembly.instantiateStreaming(fetch(fileName), importObj)
+    .then(results => {
+      console.log(results.instance.exports._doubler(2))
+    });
 }
+
+export default useWasm;
