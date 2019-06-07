e7aaedd7957601778b147c7d6823f919a3990341
Raphael Amorim <rapha850@gmail.com>
2019-06-07 18:35:50 -0300

    emscripten build command

--- emscripten/index.js
+++ emscripten/index.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 
 const argv = process.argv;
+const path = require('path');
 
 function guide(argument) {
   console.log('\nAvailable commands: install, build, version\n Docs: https://github.com/raphamorim/wasm')
@@ -18,8 +19,16 @@ switch (command) {
 
   case 'build':
     if (!argv[3] || !argv[4] || !argv[5]) {
-      return guide();
+      const pkgPath = path.resolve(process.cwd(), 'emscripten.config.js');
+      console.log('loading from ' + pkgPath);
+      const build = require('./build');
+      const files = require(pkgPath).forEach(file => {
+        build(process.cwd(), file.input, file.functions, file.output)
+      })
+
+      return 0;
     }
+
     require('./build')(process.cwd(), argv[3], argv[4], argv[5])
     break;
 
